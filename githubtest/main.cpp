0a71f2060cc9afcf16950d7f483fee57054352d7
zzimni <hjm3327@naver.com>
2021-07-14 08:47:30 +0900

    Initial Commit

---
+++ githubtest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  githubtest
+//
+//  Created by USER on 2021/07/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
